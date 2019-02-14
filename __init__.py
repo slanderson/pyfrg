fa05b5de62512bcaed6f9d7641744bf04858a7d8
Spenser Anderson <spenseranderson@gmail.com>
2019-02-13 16:23:27 -0800

    add vector stacking

--- __init__.py
+++ __init__.py
@@ -1,1 +1,5 @@
-from pyfrg.xpost_utils import load_xpost, load_node_positions_top
+from pyfrg.xpost_utils import (load_xpost, 
+                               stack_solution_vectors,
+                               unstack_solution_vectors,
+                               load_node_positions_top,
+                              )
