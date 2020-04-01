3ac930304bf58c6c298d6b2b2d19d96badd98ef1
Spenser Anderson <aspenser@sh02-ln01.stanford.edu>
2020-04-01 14:25:46 -0700

    Fix whitespace inconsistencies

--- xpost_utils.py
+++ xpost_utils.py
@@ -17,51 +17,51 @@ class NotImplementedError(Exception):
         self.message = message
 
 
-
 def load_xpost(fname):
-  """Loads an xpost file into a numpy array
+    """Loads an xpost file into a numpy array
 
-  Args: 
+    Args: 
     fname (string): a string holding the path to the xpost file to be loaded
 
-  Returns:
+    Returns:
     ndarray: a 3D ndarray holding the xpost data, with shape (num_nodes, dim_of_state,
       num_vectors)
     ndarray: a 1D ndarray holding the numeric tags associated with each vector in the
       xpost file
-  
-  """
-  with open(fname) as f:
 
-    # get number of nodes, vector dimension, and number of vectors
-    f.readline()  # ignore header
-    num_nodes = int(f.readline())
-    f.readline()  # skip to first data point
-    first_data = f.readline().split()
-    dim = len(first_data)
-    num_lines = sum(1 for _ in f) + 4 # we've already seen 4 lines
-    num_vecs = int( (num_lines - 2) / (num_nodes + 1) )
-    f.seek(0)
+    """
 
-    data = np.empty((num_nodes, dim, num_vecs))
-    tag_list = []
+    with open(fname) as f:
+
+        # get number of nodes, vector dimension, and number of vectors
+        f.readline()  # ignore header
+        num_nodes = int(f.readline())
+        f.readline()  # skip to first data point
+        first_data = f.readline().split()
+        dim = len(first_data)
+        num_lines = sum(1 for _ in f) + 4 # we've already seen 4 lines
+        num_vecs = int( (num_lines - 2) / (num_nodes + 1) )
+        f.seek(0)
+
+        data = np.empty((num_nodes, dim, num_vecs))
+        tag_list = []
+        
+        for _ in range(2): f.readline()  # skip header lines
+        for vec in range(num_vecs):
+
+            tag_str = f.readline() 
+            tag_num = None
+            try:
+                tag_num = int(tag_str)
+            except ValueError:
+                tag_num = float(tag_str)
+            tag_list += [tag_num]
+
+            for node in range(num_nodes):
+                data[node, :, vec] = [float(i) for i in f.readline().split()]
     
-    for _ in range(2): f.readline()  # skip header lines
-    for vec in range(num_vecs):
-
-      tag_str = f.readline() 
-      tag_num = None
-      try:
-        tag_num = int(tag_str)
-      except ValueError:
-        tag_num = float(tag_str)
-      tag_list += [tag_num]
-
-      for node in range(num_nodes):
-        data[node, :, vec] = [float(i) for i in f.readline().split()]
-    
-  tags = np.array(tag_list)
-  return data, tags 
+    tags = np.array(tag_list)
+    return data, tags 
 
 
 def write_xpost(fname, vecs, tags):
@@ -100,72 +100,72 @@ def write_xpost(fname, vecs, tags):
 
 
 def stack_solution_vectors(sol_vecs):
-  """Turns a 3D array of solution vectors into a 2D array by flattening solution vectors
+    """Turns a 3D array of solution vectors into a 2D array by flattening solution vectors
 
-  Running load_xpost() on a file containing several solution vectors will give a 3D array,
-  which must be indexed by both the node number and the dimension-index into the state
-  vector.  Then the third dimension indexes into a chosen 2D solution vector.  But it is
-  often desirable to stack each solution vector into an actual 1D vector, where each
-  element represents a DOF (a specific state vector value at a specific node).  For
-  instance, to run an SVD on a set of solution vectors, the vectors all need to be in a
-  normal 2D matrix.
+    Running load_xpost() on a file containing several solution vectors will give a 3D array,
+    which must be indexed by both the node number and the dimension-index into the state
+    vector.  Then the third dimension indexes into a chosen 2D solution vector.  But it is
+    often desirable to stack each solution vector into an actual 1D vector, where each
+    element represents a DOF (a specific state vector value at a specific node).  For
+    instance, to run an SVD on a set of solution vectors, the vectors all need to be in a
+    normal 2D matrix.
 
-  Args:
+    Args:
     sol_vecs (ndarray): a 3D ndarray with shape (num_nodes, dim_of_state, num_vectors)
 
-  Returns:
+    Returns:
     ndarray: A 2D array with shape (num_nodes*dim_of_state, num_vectors)
-  """
-  num_nodes = sol_vecs.shape[0]
-  dim = sol_vecs.shape[1]
-  num_vecs = sol_vecs.shape[2]
+    """
+    num_nodes = sol_vecs.shape[0]
+    dim = sol_vecs.shape[1]
+    num_vecs = sol_vecs.shape[2]
 
-  return sol_vecs.reshape((num_nodes*dim, num_vecs))
+    return sol_vecs.reshape((num_nodes*dim, num_vecs))
   
 
 def unstack_solution_vectors(stacked_vecs, dim=5):
-  """Unstacks a 2D matrix of solution vectors into a 3D array indexable by state-vector dimension
+    """Unstacks a 2D matrix of solution vectors into a 3D array indexable by state-vector dimension
 
-  Sets of solution vectors can be most intuitively accessed when the array is 3D, with
-  separate indices for node number, state-vector dimension, and vector number.  But to do
-  linear algebra on a matrix of solution vectors, they must be stored as a 'matrix' 2D
-  array, indexed by DOF.  This function transforms from a matrix of solution vectors
-  indexed by DOF, to a 3D array indexed by node number and state-vector dimension. 
+    Sets of solution vectors can be most intuitively accessed when the array is 3D, with
+    separate indices for node number, state-vector dimension, and vector number.  But to do
+    linear algebra on a matrix of solution vectors, they must be stored as a 'matrix' 2D
+    array, indexed by DOF.  This function transforms from a matrix of solution vectors
+    indexed by DOF, to a 3D array indexed by node number and state-vector dimension. 
 
-  Args:
+    Args:
     stacked_vecs (ndarray): a 2D ndarray with shape (num_nodes*dim_of_state, num_vectors)
     dim (int): the dimension of the state vector at each node (e.g. 5 for inviscid 3D
       simulations)
 
-  Returns:
+    Returns:
     ndarray: A 3D array with shape (num_nodes, dim_of_state, num_vectors)
-  """
-  num_nodes = int( stacked_vecs.shape[0] / dim )
-  num_vecs = stacked_vecs.shape[1]
+    """
+    num_nodes = int( stacked_vecs.shape[0] / dim )
+    num_vecs = stacked_vecs.shape[1]
 
-  return stacked_vecs.reshape( (num_nodes, dim, num_vecs) )
+    return stacked_vecs.reshape( (num_nodes, dim, num_vecs) )
 
 
 def load_node_positions_top(fname):
-  """Loads the node positions from a top file
+    """Loads the node positions from a top file
 
-  Args: 
+    Args: 
     fname (string): a string holding the path to the top file whose node positions should
       be loaded
 
-  Returns:
+    Returns:
     ndarray: a 2D ndarray with shape (num_nodes, 3) holding the node positions
-  
-  """
-  with open(fname) as f:
 
-    f.readline()  # ignore header
-    positions_list = []
-    for line in f:
-      if line.lstrip()[0].isalpha(): break
-      positions_list += [[float(i) for i in line.split()[1:]]]
+    """
+    with open(fname) as f:
+
+        f.readline()  # ignore header
+        positions_list = []
+        for line in f:
+            if line.lstrip()[0].isalpha(): break
+            positions_list += [[float(i) for i in line.split()[1:]]]
       
-  return np.array(positions_list)
+    return np.array(positions_list)
 
 
 def conservative_to_primitive(cons_mat, 
